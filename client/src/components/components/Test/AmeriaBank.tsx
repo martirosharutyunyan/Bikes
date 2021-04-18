a894a8fd7ff801553dfa0a32fc234026c1d889fa
martiros888 <harutunyan.martiros@mail.ru>
2021-04-18 15:41:57 +0400

    add

--- client/src/components/components/Test/AmeriaBank.tsx
+++ client/src/components/components/Test/AmeriaBank.tsx
@@ -39,9 +39,9 @@ const Test: FC = () => {
                     <section>
                         <form action="https://banking.idram.am/Payment/GetPayment" method="POST">
                             <input type="hidden" name="EDP_LANGUAGE" value="EN"/>
-                            <input type="hidden" name="EDP_REC_ACCOUNT" value="100000114" />
+                            <input type="hidden" name="EDP_REC_ACCOUNT" value="110000803" />
                             <input type="hidden" name="EDP_DESCRIPTION" value="Order description" />
-                            <input type="hidden" name="EDP_AMOUNT" value="1900" />
+                            <input type="hidden" name="EDP_AMOUNT" value="1" />
                             <input type="hidden" name="EDP_BILL_NO" value ="1806"/>
                             <input type="submit" value="submit"/>
                         </form>
